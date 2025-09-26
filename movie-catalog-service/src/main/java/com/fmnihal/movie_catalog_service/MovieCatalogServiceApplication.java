450032bd0c0a7b92715fa581a59b68fde6f21139
fmnihal <fattahmahmudnihal@gmail.com>
2025-09-27 04:39:40 +0600

    generated movie-catalog-service using spring initializr

--- movie-catalog-service/src/main/java/com/fmnihal/movie_catalog_service/MovieCatalogServiceApplication.java
+++ movie-catalog-service/src/main/java/com/fmnihal/movie_catalog_service/MovieCatalogServiceApplication.java
@@ -1,14 +1,13 @@
-package com.fmnihal.service_registry;
+package com.fmnihal.movie_catalog_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableEurekaServer
-public class ServiceRegistryApplication {
+public class MovieCatalogServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ServiceRegistryApplication.class, args);
+		SpringApplication.run(MovieCatalogServiceApplication.class, args);
 	}
 
 }
