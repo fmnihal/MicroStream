5de0a13802cbf8627bc1b82cafd5e23f454fe0c2
fmnihal <fattahmahmudnihal@gmail.com>
2025-09-27 05:59:44 +0600

    modified movie-streaming-service

--- movie-streaming-service/src/main/java/com/fmnihal/movie_streaming_service/MovieStreamingServiceApplication.java
+++ movie-streaming-service/src/main/java/com/fmnihal/movie_streaming_service/MovieStreamingServiceApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MovieStreamingServiceApplication {
 
 	public static void main(String[] args) {
