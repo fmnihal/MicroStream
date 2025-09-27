f86f86dd6c6378caea76ef1486db80d0d4cf3936
fmnihal <fattahmahmudnihal@gmail.com>
2025-09-27 09:48:16 +0600

    reduced error count from 29 to 19

--- movie-catalog-service/src/main/java/com/fmnihal/movie_catalog_service/model/MovieInfo.java
+++ movie-catalog-service/src/main/java/com/fmnihal/movie_catalog_service/model/MovieInfo.java
@@ -13,35 +13,29 @@ public class MovieInfo {
     private Long id;
     private String name;
     private String path;
+    private String description;
 
     public Long getId() {
         return id;
     }
-
     public void setId(Long id) {
         this.id = id;
     }
-
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public String getDescription() {
         return description;
     }
-
     public void setDescription(String description) {
         this.description = description;
     }
-
     public String getPath() {
         return path;
     }
-
     public void setPath(String path) {
         this.path = path;
     }
